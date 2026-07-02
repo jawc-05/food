b93c3d26bf359a43510b2f763d510bd44cd45685
jawc-05 <joaoalfredowc@gmail.com>
2026-07-02 23:20:13 +0200

    feat: adding Hero component

---
+++ efood/src/components/Hero/index.jsx
@@ -0,0 +1,14 @@
+    import React from 'react';
+    import { HeroContainer } from './styles';
+
+    import heroImg from '../../assets/hero-completo.png'; 
+
+    function Hero() {
+    return (
+        <HeroContainer>
+        <img src={heroImg} alt="efood - Viva experiências gastronômicas no conforto da sua casa" />
+        </HeroContainer>
+    );
+    }
+
+export default Hero;
---
+++ efood/src/components/Hero/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+    export const HeroContainer = styled.header`
+    width: 100%;
+    display: block;
+    
+    img {
+        width: 100%;
+        height: auto;
+        max-height: 384px; 
+        object-fit: cover;
+        display: block;
+    }
+`;
