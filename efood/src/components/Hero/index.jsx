382102053029e35bb8e0315ef8eaaab4b0f4a8cd
jawc-05 <joaoalfredowc@gmail.com>
2026-07-04 18:20:47 +0200

    fix: fixing hero name

--- efood/src/components/Hero/index.jsx
+++ efood/src/components/Hero/index.jsx
@@ -1,7 +1,7 @@
     import React from 'react';
     import { HeroContainer } from './styles';
 
-    import heroImg from '../../assets/hero.png'; 
+    import heroImg from '../../assets/hero1.png'; 
 
     function Hero() {
     return (
